4b63db50a459373a335ebc701da9b22fd0e60c9a
shraddhas1908 <Shraddhashinde1908@gmail.com>
2018-06-23 12:36:09 +0530

    Netwrking
    Shraddha

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>
+    Welcome to {{ title }}!
+  </h1>
+  <h2>
+    Educating for better tomorrow
+  </h2>
+  
+  `,
+  
+})
+export class AppComponent {
+  title = 'Marvellous Infosystem';
+}
